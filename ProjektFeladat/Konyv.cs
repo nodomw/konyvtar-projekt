1243f2f362592d83b4e59d3bdb763b85e357dad1
wintea <nodomatyi@gmail.com>
2024-01-03 21:29:09 +0100

    Add project files.

---
+++ ProjektFeladat/Konyv.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektFeladat
+{
+    internal class Konyv // Könyv osztály
+    {
+        public string title { get; set; } // Cím
+        public string author { get; set; } // Szerző
+
+        public string genre { get; set; } // Műfaj
+
+        public int year { get; set; } // Kiadás éve
+
+        public string url { get; set; } // Elérhetési URL
+    }
+    internal class Konyvek
+    {
+        public List<Konyv> books { get; set; } // List<Konyv>-et nem engedte a deszerializálásnál, ezért kellett ez.
+    }
+}
---
+++ ProjektFeladat/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+// usage: booksearcher.exe [file] [query]
+using ProjektFeladat;
+using Spectre.Console;
+using YamlDotNet.Serialization;
+StreamReader file; // fájl arg
+
+// fájlbeolvasós try-catch
+try
+{
+    file = new(args[0]);
+}
+catch (Exception e)
+{
+    throw e;
+}
+// YAML deszeriálizáló létrehozása, és a beolvasott lista deszerializálása
+var deserializer = new DeserializerBuilder().Build();
+var deserializedBook = deserializer.Deserialize<Konyvek>(file.ReadToEnd()); // kiolvasott adatok átírása 'Konyvek' osztályba
+
+// Összes könyv adatának átírása listába
+List<Konyv> Books = new();
+foreach (var item in deserializedBook.books)
+{
+    Books.Add(item);
+}
+
+AnsiConsole.MarkupLine("[bold yellow]Welcome![/]\n[blue]Leave the prompt empty to list all books.[/]\n[red]Press CTRL+C to exit[/]");
+
+// main loop
+while (true)
+{
+    // tábla GUI előkészítése
+    var table = new Table();
+
+    table.AddColumn("Title");
+    table.AddColumn("Year");
+    table.AddColumn("Author");
+    table.AddColumn("Genre");
+    table.AddColumn("URL");
+    table.Border(TableBorder.Rounded);
+
+    string search = AnsiConsole.Prompt(new TextPrompt<string>("[bold]enter search term: [/]").AllowEmpty()); // keresés
+
+    for (int i = 0; i < Books.Count; i++)
+    {
+        if (Books[i].title.IndexOf(search, StringComparison.OrdinalIgnoreCase) >= 0) // indexes keresés a 'search' változó alapján, nagybetűket ignorálva. a keresés csak a cím alapján működik.
+        {
+            var konyv = Books[i]; // Jelenlegi könyv változó
+
+            // egy sor hozzáadása az i-edik könyvvel
+            table.AddRow(konyv.title, konyv.year.ToString(), konyv.author, konyv.genre, $"[blue underline]{konyv.url}[/]");
+        }
+    }
+    
+    AnsiConsole.Write(table); // tábla kiírása
+}
