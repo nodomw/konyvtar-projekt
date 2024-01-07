63756e0845e9ddb306ad476877d953f042222548
wintea <nodomatyi@gmail.com>
2024-01-07 15:46:26 +0100

    Command class

--- ProjektFeladat/Command.cs
+++ ProjektFeladat/Command.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ProjektFeladat
@@ -15,6 +17,14 @@ namespace ProjektFeladat
             Add,
             Remove,
         };
-        public Command() { }
+        public Commands StringToCommand(string command)
+        {
+            if (command == null) throw new ArgumentNullException(nameof(command));
+
+            if (command[1] == 'q') return Commands.Search; // 'q' to Search
+            else if (command[1] == 'a') return Commands.Add; // 'a' to Add
+            else if (command[1] == 'r') return Commands.Remove; // 'r' to Remove
+            else return Commands.None; // None to anything else.
+        }
     }
 }
