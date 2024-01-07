4803e8859e88601a01f951c4269c57a46d029984
wintea <nodomatyi@gmail.com>
2024-01-07 16:06:08 +0100

    command classes, empty methods

--- ProjektFeladat/Command.cs
+++ ProjektFeladat/Command.cs
@@ -1,23 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using YamlDotNet.Serialization;
 
 namespace ProjektFeladat
 {
+    internal enum Commands
+    {
+        None,
+        Search,
+        Add,
+        Remove,
+    };
     internal class Command
     {
-        public enum Commands
-        {
-            None,
-            Search,
-            Add,
-            Remove,
-        };
-        public Commands StringToCommand(string command)
+        internal Commands ConvertCmd(string command)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
 
@@ -26,5 +28,9 @@ namespace ProjektFeladat
             else if (command[1] == 'r') return Commands.Remove; // 'r' to Remove
             else return Commands.None; // None to anything else.
         }
+        internal void Search(Konyvek books) { }
+        internal void Remove(Konyv book) { }
+        internal void Add(Konyv book) { }
+        internal void Save() { }
     }
 }
