469eb83c7d940c40ebcaba29949dac2468b5ee42
wintea <nodomatyi@gmail.com>
2024-01-07 15:39:08 +0100

    add command class

---
+++ ProjektFeladat/Command.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektFeladat
+{
+    internal class Command
+    {
+        public enum Commands
+        {
+            None,
+            Search,
+            Add,
+            Remove,
+        };
+        public Command() { }
+    }
+}
